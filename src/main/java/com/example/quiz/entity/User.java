a94c8248cc8cc7e20fec3a5829403920f72b46bc
Jerrylac <j78779915@gmail.com>
2023-12-13 17:59:33 +0800

    20231213 Java

---
+++ src/main/java/com/example/quiz/entity/User.java
@@ -0,0 +1,45 @@
+package com.example.quiz.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+	@Id
+	@Column(name="account")
+	private String account;
+	
+	@Column(name="password")
+	private String pwd;
+
+	public User() {
+		super();
+	}
+
+	public User(String account, String pwd) {
+		super();
+		this.account = account;
+		this.pwd = pwd;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+}
